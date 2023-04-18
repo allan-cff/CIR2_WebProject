6af8ab2e6aa9c7ccf03127150e187a348a5cfcab
Quentin Le Goff <quentin.le-goff@isen-ouest.yncrea.fr>
2023-04-18 11:00:27 +0200

    changed dbconnect

--- connect.php
+++ connect.php
@@ -1,17 +1,13 @@
-<?php
-    include 'constants.php' ;
-
-    function dbConnect(){
-
-        $dsn = 'pgsql:dbname='.DB_NAME.';host='.DB_SERVER.';port='.DB_PORT;
-        $user = DB_USER;
-        $password = DB_PASSWORD;
-
-        try{
-            $conn = new PDO($dsn, $user, $password);
-            return $conn;
-        } catch (PDOException $e){
-        echo 'Connexion échouée : ' . $e->getMessage();
-        }
+<?php 
+include "constants.php";
+function dbConnect(){
+    $dsn = "pgsql:dbname=". DB_NAME . ";host=" . DB_SERVER . ";port=" . DB_PORT;
+    try{
+        $conn = new PDO($dsn, DB_USER, DB_PASSWORD);
+        echo 'Connected to database';
     }
-?>
+    catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+    return $conn;
+}
