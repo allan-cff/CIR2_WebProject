896bfa9f8ead82cfc5cc433ca51976e29df6bebd
Allan Cueff <allan.cueff@isen-ouest.yncrea.fr>
2023-04-19 17:30:40 +0200

    fix getTeachers & test.php + remove test.php from gitignore

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
     error_reporting(E_ALL);
     $conn = dbConnect();
     addStudent($conn, "patoche@isen.com", "Coucou", "Patoche", "test", "0616155976", "CIR3");
-    addStudent($conn, "patoche2@isen.com", "Coucou", "Patoche", "test", "0616155976", "CIR3");
+    addStudent($conn, "patoche2@isen.com", "Coucou", "Patoche", "test", "0616155977", "CIR3");
     $students = getStudents($conn);
     echo "<b>ETUDIANTS</b><br>";
     foreach($students as $student){
@@ -12,20 +12,21 @@
     }
     deleteUser($conn, "patoche@isen.com");
     echo "<b>KILL PATOCHE</b><br>";
-    $students = getStudents($conn);
+    $students = getAllStudents($conn);
     foreach($students as $student){
         echo $student['mail']. '<br>';
     }
     echo "<b>PROFESSEURS</b><br>";
-    addTeacher($conn, 'patrick@isen.com', 'Patrick', 'Pat', 'test', '0616155976');
-    addTeacher($conn, 'patrick3@isen.com', 'nik', 'la', 'test', '0616155976');
-    addTeacher($conn, 'erin@isen.com', 'erin', 'nazi', 'test', '0616155977');
-    $teachers = getTeachers($conn);
+    addTeacher($conn, 'patrick@isen.com', 'Patrick', 'Pat', 'test', '0616155978');
+    addTeacher($conn, 'patrick3@isen.com', 'nik', 'la', 'test', '0616155979');
+    addTeacher($conn, 'erin@isen.com', 'erin', 'nazi', 'test', '0616155980');
+    $teachers = getAllTeachers($conn);
     foreach($teachers as $teacher){
         echo $teacher['mail']. '<br>';
     }
     echo "<b>ADMINS</b><br>";
     addAdmin($conn, 'quentin@isen.com', 'Quentin', 'Le Goff', 'test', '0750230013');
-    $admin = getAdmin($conn);
+    addAdmin($conn, 'allan@isen.com', 'Allan', 'Cueff', 'test', '0750230014');
+    $admin = getAllAdmins($conn);
     echo $admin['mail']. '<br>';
 ?>
