ab4115701ef55d8b59ee87a63e2029a995bf8773
Quentin Le Goff <quentin.le-goff@isen-ouest.yncrea.fr>
2023-05-15 13:36:34 +0200

    add_semester committed

--- private/admin/admin_add_lesson.php
+++ private/admin/admin_add_lesson.php
@@ -117,11 +117,42 @@
     </nav>
   </header>
 
+  <?php
+    if(isset($_POST['new_lesson']) && isset($_POST['class_add_lesson']) && isset($_POST['semester_add_lesson']) && isset($_POST['teacher_add_lesson']) && isset($_POST['matter_add_lesson'])){
+      try {
+        $user->addLesson($_POST['matter_add_lesson'], $_POST['teacher_add_lesson'], $_POST['class_add_lesson'], $_POST['semester_add_lesson']);
+        echo '
+        <div class="container">
+          <div class="alert alert-success d-flex align-items-center alert-dismissible fade show" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </svg>  
+            &nbsp;Cours ajouté avec succès !
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+        </div>
+        ';
+      } catch(Exception $e) {
+        echo '
+        <div class="container">
+          <div class="alert alert-danger d-flex align-items-center alert-dismissible fade show" role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+            </svg>
+              &nbsp;Erreur durant l\'ajout du cours
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+        </div>  
+        ';
+      }
+    }
+  ?>
+
 
   <main>
     <div class="container">
       <div class="row">
-        <form class="col-md-7 offset-md-3" method="post" action="?????.php">
+        <form class="col-md-7 offset-md-3" method="post" action="admin_add_lesson.php">
           <div class="input-group mb-3">
             <label for="exampleFormControlInput1" class="col-sm-3 col-form-label">Classe</label>
 
@@ -160,7 +191,7 @@
                 <?php
                   $teachers = $user->listTeachers();
                   foreach ($teachers as $teacher) {
-                    echo '<option value="'.$teacher->id.'">'.$teacher->id.' '.$teacher->name.'</option>';
+                    echo '<option value="'.$teacher->mail.'">'.$teacher->getFullName().'</option>';
                   }
                 ?>
               </select>
